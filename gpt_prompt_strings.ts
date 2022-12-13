7d385208ca33c51188e8dd317dea8fd932d66719
ahlee42 <ahl1395@gmail.com>
2022-12-13 00:27:22 -0500

    Add files via upload

---
+++ gpt_prompt_strings.ts
@@ -0,0 +1,50 @@
+export const prompt_beginning = "Write a ";
+export const prompt_style: string[] = ['rap', 'song', 'poem', 'speech', 'haiku', 'sonnet']
+export const prompt_middle = "by ";
+export const prompt_narrators: string[] = 
+    [
+        'Barack Obama', 
+        'Taylor Swift', 
+        'Ariana Grande', 
+        'Justin Bieber', 
+        'Donald Trump', 
+        'Bill Clinton', 
+        'Elon Musk',
+        'Kanye West',
+        'Chris Rock',
+        'Dave Chapelle',
+        'Katy Perry',
+        'Rihanna',
+        'Narendra Modi',
+        'Lady Gaga',
+        'Selena Gomez',
+        'Britney Spears',
+        'Demi Lovato',
+        'Drake',
+        'Miley Cyrus',
+        'Jimmmy Fallon',
+        'Winston Churchill',
+        'Bill Gates',
+        'Abraham Lincoln',
+        'Thomas Jefferson',
+        'John F. Kennedy',
+        'Aristotle',
+        'George Washington',
+        'Napoleon',
+        'William Shakespeare',
+        'Cleopatra',
+        'Julius Caesar',
+        'Alexander the Great',
+        'a 14 year old girl with ADHD',
+        'a 60 year old man who has lived through several financial bubbles',
+        'a 87 year old Japanese man who runs a pizza shop',
+        'a vegan crypto billionaire'
+    ];
+export const prompt_end: string[] = 
+    [
+        ' that compares not paying royalties on digital art to stubbing your toe.',
+        ' that compares not paying royalties on digital art to tripping on a banana.',
+        ' that compares not paying royalties on digital art to farting in an elevator.',
+        ' that scolds the users for not paying royalties on digital art like a caring grandma.',
+        
+    ]
