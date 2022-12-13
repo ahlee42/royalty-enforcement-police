67f5c5fb47b86031f2e6a5f3c71451c571f61e88
Arnold Lee <ahl1395@gmail.com>
2022-12-13 02:00:18 -0500

    more prompts, map of twitter addresses

---
+++ mint_to_collection_twitter_map.js
@@ -0,0 +1,11 @@
+// on-chain collection address : twitter handle
+
+var dict = {
+    "4mKSoDDqApmF1DqXvVTSL6tu2zixrSSNjqMxUnwvVzy2": "@y00tsNFT",
+    "6XxjKYFbcndh2gDcsUrmZgVEsoDxXMnfsaGY6fpTJzNr": "@DeGodsNFT",
+    "6mszaj17KSfVqADrQj3o4W3zoLMTykgmV37W4QadCczK": "@Claynosaurz",
+    "SMBH3wF6baUj6JWtzYvqcKuj2XCKWDqQxzspY12xPND": "@SolanaMBS",
+    "3saAedkM9o5g1u5DCqsuMZuC4GRqPB4TuMkvSsSVvGQ3": "@OkayBears",
+    "ES2iF5ctjqvtopPn4n6K7c9fdHjYg41rYXL2XzJK37jF": "@ABC123Community",
+    "BUjZjAS2vbbb65g7Z1Ca9ZRVYoJscURG5L3AkVvHP9ac": "@FamousFoxFed",
+}
