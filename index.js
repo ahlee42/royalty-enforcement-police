e3ebe6c68e6c81a7b45017c3403e3bb777499a35
JWC <103419525+DataRaptor@users.noreply.github.com>
2022-12-13 01:29:18 -0500

    Added error handling.

--- index.js
+++ index.js
@@ -45,62 +45,73 @@ const generateGpt3Prompt = (feePayer) => {
 const getGpt3Dialog = async(feePayer) => {
     const temperature = Math.random()
     const prompt = generateGpt3Prompt(feePayer)
-    const response = await axios.post(
-        `${GPT3_API_URL}/v1/completions`,
-        {
-            model: "text-davinci-003",
-            prompt,
-            max_tokens: 4000,
-            temperature
-        },
-        { headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${GPT3_API_KEY}`
-        } 
-    },     
-    )
-    if (response.status == 200){
-        const { choices } = response.data
-        const dialog = choices[0]
-        return dialog.text
-    }
-    throw Error(`Could not getGpt3Dialog with feePayer: ${feePayer} and prompt: ${prompt}.`)
+    try{
+        const response = await axios.post(
+            `${GPT3_API_URL}/v1/completions`,
+            {
+                model: "text-davinci-003",
+                prompt,
+                max_tokens: 4000,
+                temperature
+            },
+            { headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${GPT3_API_KEY}`
+            } 
+        },     
+        )
+        if (response.status == 200){
+            const { choices } = response.data
+            const dialog = choices[0]
+            return dialog.text
+        }
+    } catch {}
+    throw Error(`Could not getGpt3Dialog with feePayer: ${feePayer} and prompt: ${prompt} w/ error: ${error.message}.`)
 }
-
-const app = express()
-
-app.use(express.json())
-app.use(bodyParser.urlencoded({
-    extended: true,
-}))
-app.use(cors())
 
 app.post("/", async(req, res) => {
 
-    const nfts = req.body[0]?.events?.nft?.nfts
+    try{
+        const nfts = req.body[0]?.events?.nft?.nfts
 
-    const firstNftMint = nfts[0]
-
-    const mint= firstNftMint?.mint
-    const { data: metadata } = await axios.post(`${HELIUS_API_URL}/v0/tokens/metadata?api-key=${HELIUS_API_KEY}`, {
-        mintAccounts: [ mint ]
-    })
-
-    const image = metadata.offChainData.image
-
-    const creators = metadata[0]?.onChainData?.data?.creators?.map(x => x.address);
-
-    const paidRoyalty = req.body[0].nativeTransfers.some(x => creators?.includes(x.toUserAccount))
-
-    const feePayer= req.body[0].feePayer
+        const firstNftMint = nfts[0]
     
-    if (paidRoyalty){
-        return 
+        const mint= firstNftMint?.mint
+        const { data: metadata } = await axios.post(`${HELIUS_API_URL}/v0/tokens/metadata?api-key=${HELIUS_API_KEY}`, {
+            mintAccounts: [ mint ]
+        })
+    
+        const image = metadata.offChainData.image
+    
+        const creators = metadata[0]?.onChainData?.data?.creators?.map(x => x.address);
+    
+        const paidRoyalty = req.body[0].nativeTransfers.some(x => creators?.includes(x.toUserAccount))
+    
+        const feePayer= req.body[0].feePayer
+        
+        if (paidRoyalty){
+            return 
+        }
+    
+        const gpt3Prompt = getGpt3Dialog(feePayer) 
+    
+        sendTweet(gpt3Prompt, "_")
+    
+    
+        res.status(200).json({
+            ok: true,
+            message: "Royalty Enforcement Police Tweet created"
+
+        })
+    } catch(error){
+        res.status(500).json({
+            ok: false,
+            message: "Royalty Enforcement Police crashes"
+        })
     }
 
-    const gpt3Prompt = getGpt3Dialog(feePayer) 
 
-    sendTweet(gpt3Prompt, "_")
+    
 
 })
 
