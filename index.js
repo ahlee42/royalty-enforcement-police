0d2536f3770ff018b4a7bf2b32da64727d40c557
JWC <103419525+DataRaptor@users.noreply.github.com>
2022-12-13 01:22:09 -0500

    Fixes.

--- index.js
+++ index.js
@@ -3,53 +3,19 @@ var axios = require("axios");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 
+
 const HELIUS_API_URL= "https://api.helius.xyz"
 const GPT3_API_URL = "https://api.openai.com"
 const PORT = parseInt(process.env.PORT || "8080") 
 const HELIUS_API_KEY= process.env.HELIUS_API_KEY || ""
 const GPT3_API_KEY= process.env.GPT3_API_KEY || ""
 
-const app = express()
-
-app.use(express.json())
-app.use(bodyParser.urlencoded({
-    extended: true,
-}))
-app.use(cors())
-
-app.post("/webhook-handler", async(req, res) => {
-
-    const nfts = req.body[0]?.events?.nft?.nfts
-
-    const firstNftMint = nfts[0]
-
-    const mint= firstNftMint?.mint
-    const { data: metadata } = await axios.post(`${HELIUS_API_URL}/v0/tokens/metadata?api-key=${HELIUS_API_KEY}`, {
-        mintAccounts: [ mint ]
-    })
-
-    const image = metadata.offChainData.image
-
-    const creators = metadata[0]?.onChainData?.data?.creators?.map(x => x.address);
-
-    const paidRoyalty = req.body[0].nativeTransfers.some(x => creators?.includes(x.toUserAccount))
-
-    const feePayer= req.body[0].feePayer
-    
-    if (paidRoyalty){
-        return 
-    }
-
-    const gpt3Prompt = getGpt3Dialog(feePayer) 
-
-    // Send out tweet here.
-
-
-
-})
-
 const generateGpt3Prompt = (feePayer) => {
-    return `write a poem about ${feePayer} who stole 10 billion dollars of client funds and commited fraud.`
+    const style = prompt_style[Math.floor(Math.random() * prompt_style.length) - 1]
+    const narrarator = prompt_narrators[Math.floor(Math.random() * prompt_narrators.length) - 1]
+    const end = prompt_end[Math.floor(Math.random() * prompt_end.length) - 1]
+    return `${prompt_beginning} ${style} narrated ${prompt_middle} ${narrarator} about ${feePayer} ${end}`
+
 }
 
 const getGpt3Dialog = async(feePayer) => {
@@ -77,14 +43,40 @@ const getGpt3Dialog = async(feePayer) => {
     throw Error(`Could not getGpt3Dialog with feePayer: ${feePayer} and prompt: ${prompt}.`)
 }
 
+const app = express()
+
+app.use(express.json())
+app.use(bodyParser.urlencoded({
+    extended: true,
+}))
+app.use(cors())
 
 app.post("/", async(req, res) => {
 
-    const shameText= await gpt3.sendMessage('Write a python version of bubble sort.')
-    
-    console.log(shameText)
+    const nfts = req.body[0]?.events?.nft?.nfts
 
-    return shameText
+    const firstNftMint = nfts[0]
+
+    const mint= firstNftMint?.mint
+    const { data: metadata } = await axios.post(`${HELIUS_API_URL}/v0/tokens/metadata?api-key=${HELIUS_API_KEY}`, {
+        mintAccounts: [ mint ]
+    })
+
+    const image = metadata.offChainData.image
+
+    const creators = metadata[0]?.onChainData?.data?.creators?.map(x => x.address);
+
+    const paidRoyalty = req.body[0].nativeTransfers.some(x => creators?.includes(x.toUserAccount))
+
+    const feePayer= req.body[0].feePayer
+    
+    if (paidRoyalty){
+        return 
+    }
+
+    const gpt3Prompt = getGpt3Dialog(feePayer) 
+
+    // Send out tweet here.
 
 })
 
